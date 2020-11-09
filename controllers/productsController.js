6701fd8ab241d3c30ce901ec9e299407f59ad576
PabloMombiela <pablomombiela@live.com.ar>
2020-11-09 10:18:37 -0300

    2da parte, Los Archivos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -35,26 +35,98 @@ const controller = {
     // Create -  Method to store
     store: (req, res) => {
 
+        let producto = { //recupero los datos del form//
+            id: products.length + 1,
+            name: req.body.name,
+            price: req.body.price,
+            discount: req.body.discount,
+            category: req.body.category,
+            description: req.body.description
+        };
+
+        // products.id = products.length + 1 // Esta linea pone el atributo ID en el producto que creamos, si no lo hacemos se suma al json sin el atributo id, y la vista DETAIL nunca los mostraria//
+        products.push(producto); //los empujo a products que es el json parseado)
+        productsJSON = JSON.stringify(products); // los empaqueto en JSON
+
+        fs.writeFileSync('data/productsDataBase.json', productsJSON); // escribo el nuevo producto en la BD.
+
+
         console.log(req.body)
 
-        res.send('viaja por POST')
+        res.redirect('/')
     },
+
+
+
+
 
     // Update - Form to edit // Muestra formulario de Edicion
     edit: (req, res) => {
-        let id = req.params.id
-        res.render('product-edit-form', { products: products, id })
+
+        res.render('product-edit-form', { products: products, id: req.params.id - 1 })
 
     },
     // Update - Method to update // Envia producto actualizado
     update: (req, res) => {
-        res.send('Viaja por PUT')
+
+
+        //const filePath = './data/productsDataBase.json';
+
+        // Pasamos el contenido de productsDataBase.json a una variable
+        //let content = fs.readFileSync(filePath, { encoding: 'utf-8' });
+
+        // Convertimos en un array para poder operar
+        //content = JSON.parse(content);
+
+
+
+        // recupero los datos del form//
+        products.forEach(function(element) {
+            if (element.id == req.params.id) {
+                //console.log(elemento)
+                element.name = req.body.name;
+                element.price = req.body.price;
+                element.discount = req.body.discount;
+                element.category = req.body.category;
+                element.description = req.body.description;
+                //element.image = filename;
+            }
+        });
+        // los empaqueto en un JSON
+        productsJSON = JSON.stringify(products);
+
+
+        // Escribimos nuevamente el archivo productsDataBase.json
+        fs.writeFileSync('data/productsDataBase.json', productsJSON);
+
+        console.log(req.body)
+        res.redirect('/');
+
+
     },
+
+
+
+
+
+
+
+
 
     // Delete - Delete one product from DB
     destroy: (req, res) => {
+        // Aca buscamos y borramos//
+        let eliminar = products.filter(function(element) {
+                return element.id != req.params.id;
+            })
+            // los empaqueto en un string
+        productsJSON = JSON.stringify(eliminar);
+
+        // Escribimos nuevamente el archivo productsDataBase.json
+        fs.writeFileSync('data/productsDataBase.json', productsJSON);
+
         console.log('se elimino el producto ' + req.params.id) // muestra por consola lo eliminado
-        res.send('El producto se elimino correctamente, bravo.')
+        res.redirect('/')
     }
 };
 
